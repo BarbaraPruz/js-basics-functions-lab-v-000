24891e34e6b3ca8afbbd2bcfad3e74e1529c63a7
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-08-24 21:19:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ function calculatesFarePrice(start, destination) {
   let distance = distanceTravelledInFeet(start, destination);
   if ( distance>400 && distance<2000 ) {
     return distance * .02;
-  } else if distance > 2000 ) {
+  } else if ( distance > 2000 ) {
     return 25;
-  } else if distance > 2500 ) {
+  } else if ( distance > 2500 ) {
     return "cannot travel that far";
   }
   else {
