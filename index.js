19d71403dec5143b037fdefa86eedf51ef90c917
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-08-24 21:02:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(location) {
-  Math.abs(location - 42)
+  return Math.abs(location - 42)
 }
 
 function distanceFromHqInFeet(location) {
-  
+
 }
